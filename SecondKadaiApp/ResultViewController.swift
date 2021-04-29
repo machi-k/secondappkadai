3491db03d5640041781692b8ac3f49d42e1100df
machi.kawamura <machi.k.818@gmail.com>
2021-04-29 18:41:54 +0900

    完成

--- SecondKadaiApp/ResultViewController.swift
+++ SecondKadaiApp/ResultViewController.swift
@@ -8,11 +8,15 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    // 受け取るためのプロパティ（変数）を宣言しておく
+    var name:String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let result = name
+        label.text = "こんにちは\(result) さん"
     }
     
 
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -8,15 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var textField: UITextField!
+    
     @IBAction func unwind(_ segue: UIStoryboardSegue) {
-        name.text = "name"
-        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
-  
-    
+   
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        // 遷移先のResultViewControllerで宣言しているx, yに値を代入して渡す
+        resultViewController.name = textField.text!
+    }
+
 }
